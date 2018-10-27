6dbf9e181ad12156c9834ad90df7a8cea904094d
kajadelight <kaja@potepels.com>
2018-10-27 12:50:02 +0200

    Nå med alicorn <3 icon. Måtte bare bytte FA source dust

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGhost } from '@fortawesome/free-solid-svg-icons';
+import { faAlicorn } from '@fortawesome/pro-light-svg-icons';
 import './Calendar.scss';
-library.add(faGhost);
+library.add(faAlicorn);
 
 
 
@@ -17,12 +17,11 @@ export default class Calendar extends React.Component {
         let dataUrl =  "http://kaja.me/wp-json/acf/v3/forsideposter?_embed";
     }
 	render() {
-        const element = <FontAwesomeIcon icon={faGhost} />
 		return (
         <div className="c_calendar">
             <div className="center-content">
                 Kaendar : )
-                Favorite Food: <FontAwesomeIcon icon="ghost" />
+                <FontAwesomeIcon className="alicorn" icon={faAlicorn} />
             </div>
         </div>
 		);
